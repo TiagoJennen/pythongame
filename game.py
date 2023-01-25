f8508e816657a38e025fdc433466fa2c7f3923a7
TiagoJennen <113349775+TiagoJennen@users.noreply.github.com>
2023-01-25 09:51:12 +0100

    head en bg

--- game.py
+++ game.py
@@ -3,3 +3,20 @@ import time
 import random
 
 delay = 0,1
+
+score = 0
+high_score = 0
+
+wn = turtle.Screen()
+wn.title("Snake game by Stijn & Tiago")
+wn.bgcolor("green")
+wn.setup(width=600, height=600)
+wn.tracer(0)
+
+head = turtle.Turtle
+head.speed(0)
+head.shape("circle")
+head.color("red")
+head.penup()
+head.goto(0, 0)
+head.direction = "stop"
