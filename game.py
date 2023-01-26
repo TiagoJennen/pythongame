555573606191a06ad588d59906ac60ccfe7ebb37
Nj1Ts <517100@vistacollege.nl>
2023-01-26 09:55:41 +0100

    update

--- game.py
+++ game.py
@@ -7,21 +7,17 @@ delay = 0.1
 score = 0
 high_score = 0
 
-wn = turtle.Screen()
-wn.title("Snake Game by Stijn & Tiago")
-wn.bgcolor("black")
-wn.setup(width=600, height=600)
-wn.tracer(0) 
+achtergrond = turtle.Screen()
+achtergrond.title("Snake Game by Stijn & Tiago")
+achtergrond.bgcolor("black")
+achtergrond.setup(width=600, height=600)
+achtergrond.tracer(0) 
+achtergrond.bgpic("regenboog achtergond.png")
 
 head = turtle.Turtle()
 head.speed(0)
-<<<<<<< Updated upstream
-head.shape("circle")
-head.color("red")
-=======
 head.shape("square")
-head.color("black")
->>>>>>> Stashed changes
+head.color("white")
 head.penup()
 head.goto(0,0)
 head.direction = "stop"
@@ -29,20 +25,18 @@ head.direction = "stop"
 food = turtle.Turtle()
 food.speed(0)
 food.shape("circle")
-food.color("aquamarine")
+food.color("crimson")
 food.penup()
 food.goto(0,100)
 
 segments = []
 
-pen = turtle.Turtle()
-pen.speed(0)
-pen.shape("circle")
-pen.color("white")
-pen.penup()
-pen.hideturtle()
-pen.goto(0, 260)
-pen.write("Score: 0  High Score: 0", align="center", font=("akiraexpanded", 24, "normal"))
+scorebord = turtle.Turtle()
+scorebord.color("white")
+scorebord.penup()
+scorebord.hideturtle()
+scorebord.goto(0, 260)
+scorebord.write("Score: 0  High Score: 0", align="center", font=("akiraexpanded", 24, "normal"))
 
 def go_up():
     if head.direction != "down":
@@ -77,14 +71,14 @@ def move():
         x = head.xcor()
         head.setx(x + 20)
 
-wn.listen()
-wn.onkeypress(go_up, "w")
-wn.onkeypress(go_down, "s")
-wn.onkeypress(go_left, "a")
-wn.onkeypress(go_right, "d")
+achtergrond.listen()
+achtergrond.onkeypress(go_up, "w")
+achtergrond.onkeypress(go_down, "s")
+achtergrond.onkeypress(go_left, "a")
+achtergrond.onkeypress(go_right, "d")
 
 while True:
-    wn.update()
+    achtergrond.update()
 
     if head.xcor()>290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
         time.sleep(1)
@@ -100,8 +94,8 @@ while True:
 
         delay = 0.1
 
-        pen.clear()
-        pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("akiraexpanded", 24, "normal")) 
+        scorebord.clear()
+        scorebord.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("akiraexpanded", 24, "normal")) 
 
 
     if head.distance(food) < 20:
@@ -112,19 +106,19 @@ while True:
         new_segment = turtle.Turtle()
         new_segment.speed(0)
         new_segment.shape("circle")
-        new_segment.color("aquamarine")
+        new_segment.color("crimson")
         new_segment.penup()
         segments.append(new_segment)
 
         delay -= 0.001
 
-        score += 10
+        score += 1
 
         if score > high_score:
             high_score = score
         
-        pen.clear()
-        pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("akiraexpanded", 24, "normal")) 
+        scorebord.clear()
+        scorebord.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("akiraexpanded", 24, "normal")) 
 
     for index in range(len(segments)-1, 0, -1):
         x = segments[index-1].xcor()
@@ -153,9 +147,9 @@ while True:
 
             delay = 0.1
         
-            pen.clear()
-            pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("akiraexpanded", 24, "normal"))
+            scorebord.clear()
+            scorebord.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("akiraexpanded", 24, "normal"))
 
     time.sleep(delay)
 
-wn.mainloop()
+achtergrond.mainloop()
